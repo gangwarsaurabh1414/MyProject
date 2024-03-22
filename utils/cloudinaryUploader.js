22a77b044695578da42be2d110b41f5916cb009e
Saurabh Gangwar <gangwarsaurabh1414@gmail.com>
2024-03-22 08:11:30 +0530

    Second Commit

--- utils/cloudinaryUploader.js
+++ utils/cloudinaryUploader.js
@@ -1,5 +1,6 @@
 const cloudinary = require('cloudinary').v2;
-exports.uploadToCloudinary = async (file, folder, height, quality){
+
+exports.uploadToCloudinary = async (file, folder, height, quality) => {
     const options = { folder };
     if (height) options.height = height;
     if (quality) options.quality = quality;
